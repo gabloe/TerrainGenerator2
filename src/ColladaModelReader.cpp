88b0db234eae5dda4eec686a88675621565dd00c
Gabriel Loewen <gabloe@microsoft.com>
2022-09-13 09:47:45 -0700

    Remove junk

--- src/ColladaModelReader.cpp
+++
@@ -1,9 +0,0 @@
-#include "ColladaModelReader.hpp"
-
-namespace models
-{
-	std::unique_ptr<models::IModel> ColladaModelReader::Read(std::istream inputStream) const
-	{
-		return std::unique_ptr<models::IModel>{nullptr};
-	}
-}
